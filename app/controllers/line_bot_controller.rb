39183c4973d60a3a0e28898f6a75f229131d6ae1
伊東和成 <itokazunari5375@icloud.com>
2022-07-31 23:44:43 +0900

    外部APIを叩く

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -1,22 +1,33 @@
+# nethttp.rb
+require 'uri'
+require 'net/http'
 class LineBotController < ApplicationController
  
   def callback
-    body = request.body.read
-    events = client.parse_events_from(body)
-    events.each do |event|
-      case event
-      when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
-          message = {
-            type: "text",
-            text: event.message["text"]
-          }
-          client.reply_message(event['replyToken'], message)
-        end
-      end
-    end
+    uri = URI('https://qiita-api.vercel.app/api/trend')
+    res = Net::HTTP.get_response(uri)
+    puts "======="
+    puts res.body if res.is_a?(Net::HTTPSuccess)
+    puts "======="
+
   end
+  # def callback
+  #   body = request.body.read
+  #   events = client.parse_events_from(body)
+  #   events.each do |event|
+  #     case event
+  #     when Line::Bot::Event::Message
+  #       case event.type
+  #       when Line::Bot::Event::MessageType::Text
+  #         message = {
+  #           type: "text",
+  #           text: event.message["text"]
+  #         }
+  #         client.reply_message(event['replyToken'], message)
+  #       end
+  #     end
+  #   end
+  # end
 
   private
  
